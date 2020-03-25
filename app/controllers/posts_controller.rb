ed8b175c96600d110d9880c39d2f1f270246f6a4
Jon Skidanov - Ubuntu <jskidanov@westmont.edu>
2020-03-24 22:39:51 -0700

    check this out mothaf

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
   
   def index
+    @posts = Posts.all.order("created_at DESC")
   end
 
   def new
